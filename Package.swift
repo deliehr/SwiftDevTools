3bb4a8a5287d5ef9ec46152d72e281fd957cef93
Dominik Liehr <dominik_liehr@me.com>
2024-05-26 19:25:01 +0200

    Fix(package): fixed swiftlint product matching

--- Package.swift
+++ Package.swift
@@ -21,7 +21,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "DevTools", dependencies: [
-            .product(name: "SwiftLint", package: "SwiftLint")
+            .product(name: "swiftlint", package: "SwiftLint"),
+            .product(name: "SwiftLintFramework", package: "SwiftLint"),
+            .product(name: "SwiftLintBuildToolPlugin", package: "SwiftLint"),
+            .product(name: "SwiftLintCommandPlugin", package: "SwiftLint")
         ]),
         .testTarget(
             name: "DevToolsTests",
