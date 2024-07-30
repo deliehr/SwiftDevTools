ccdcf25606e8bd994631e661a3d92e487ba6104d
Dominik Liehr <dominik_liehr@me.com>
2024-07-30 20:30:21 +0200

    Feat: UIImage extensions for resizing and grayscaling

---
+++ Sources/DevTools/Extensions/UIImage.swift
@@ -0,0 +1,49 @@
+//
+//  UIImage.swift
+//
+//
+//  Created by Dominik Liehr on 29.07.24.
+//
+
+import UIKit
+import AVFoundation
+
+extension UIImage {
+    func resize(to size: CGSize) -> UIImage {
+        let maxSize = CGSize(width: size.width, height: size.height)
+
+        let availableRect = AVFoundation.AVMakeRect(
+            aspectRatio: self.size,
+            insideRect: .init(origin: .zero, size: maxSize)
+        )
+        let targetSize = availableRect.size
+
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        let renderer = UIGraphicsImageRenderer(size: targetSize, format: format)
+
+        let resized = renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+
+        return resized
+    }
+
+    func convertToGrayScale() -> UIImage? {
+        let imageRect:CGRect = CGRect(x:0, y:0, width:self.size.width, height: self.size.height)
+        let colorSpace = CGColorSpaceCreateDeviceGray()
+        let width = self.size.width
+        let height = self.size.height
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
+        let context = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+        if let cgImg = self.cgImage {
+            context?.draw(cgImg, in: imageRect)
+            if let makeImg = context?.makeImage() {
+                let imageRef = makeImg
+                let newImage = UIImage(cgImage: imageRef)
+                return newImage
+            }
+        }
+        return UIImage()
+    }
+}
