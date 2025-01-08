1df8908b9f6351508a053aa50dd6ab04909d7a50
Dominik Liehr <dominik@liehr.dev>
2025-01-08 09:38:47 +0100

    Feat: updated version of network monitor

--- Sources/DevTools/Networking/NetworkMonitor.swift
+++ Sources/DevTools/Networking/NetworkMonitor.swift
@@ -7,30 +7,77 @@
 
 import Foundation
 import Network
+import Combine
+import SwiftUI
 
 @available(iOS 17, OSX 14, *)
+@MainActor
 @Observable
-public final class NetworkMonitor {
-    public var isConnected = false
-    public var isCellular = false
-
-    private let nwMonitor = NWPathMonitor()
-    private let workerQueue = DispatchQueue.global()
-
-    public static var shared = NetworkMonitor()
-
-    public func start() {
-        nwMonitor.pathUpdateHandler = { [weak self] path in
-            DispatchQueue.main.async {
-                self?.isConnected = path.status == .satisfied
-                self?.isCellular = path.usesInterfaceType(.cellular)
+final class NetworkMonitor {
+    private let networkMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue(label: "NetworkMonitor", qos: .userInitiated)
+    
+    var isConnected: Bool? = nil
+    var usedInterface: NWInterface.InterfaceType? = nil
+    
+    @ObservationIgnored
+    var isConnectedPub = PassthroughSubject<Bool,Never>()
+    
+    @ObservationIgnored
+    var usedInterfacePub = PassthroughSubject<NWInterface.InterfaceType,Never>()
+    
+    static let shared = NetworkMonitor()
+    
+    private init() {
+        networkMonitor.pathUpdateHandler = { path in
+            Task.detached(priority: .userInitiated) { [path] in
+                let newStatus = path.status == .satisfied
+                await self.updateConnectedIfNecessary(newStatus)
+                
+                
+                let usesCellular = path.usesInterfaceType(.cellular)
+                let usesWifi = path.usesInterfaceType(.wifi)
+                let usesEthernet = path.usesInterfaceType(.wiredEthernet)
+                
+                if usesCellular {
+                    await self.updateUsedInterfaceIfNecessary(.wiredEthernet)
+                } else if usesWifi {
+                    await self.updateUsedInterfaceIfNecessary(.wifi)
+                } else if usesEthernet {
+                    await self.updateUsedInterfaceIfNecessary(.wiredEthernet)
+                } else {
+                    await self.updateUsedInterfaceIfNecessary(.other)
+                }
             }
         }
-
-        nwMonitor.start(queue: workerQueue)
+        
+        networkMonitor.start(queue: workerQueue)
     }
-
+    
+    func updateConnectedIfNecessary(_ connected: Bool) {
+        guard self.isConnected != connected else { return }
+        
+        self.isConnected = connected
+        isConnectedPub.send(connected)
+    }
+    
+    func updateUsedInterfaceIfNecessary(_ interface: NWInterface.InterfaceType) {
+        guard self.usedInterface != interface else { return }
+        
+        self.usedInterface = interface
+        self.usedInterfacePub.send(interface)
+    }
+    
+    public func start() {
+        networkMonitor.start(queue: workerQueue)
+        isConnectedPub = PassthroughSubject<Bool,Never>()
+        usedInterfacePub = PassthroughSubject<NWInterface.InterfaceType,Never>()
+    }
+    
     public func stop() {
-        nwMonitor.cancel()
+        networkMonitor.cancel()
+        isConnectedPub.send(completion: .finished)
+        usedInterfacePub.send(completion: .finished)
     }
 }
+
